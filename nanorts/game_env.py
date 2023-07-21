4259d4a0562e7acdcfd984de12306a55964690dc
dmcatte <dmcatte1995@gmail.com>
2023-07-21 20:07:51 +0100

    helloworld

--- nanorts/game_env.py
+++ nanorts/game_env.py
@@ -6,7 +6,7 @@ import numpy as np
 from nanorts.render import Render
 
 class GameEnv:
-    def __init__(self, map_paths, reward_wrights, max_steps, skip_frames=5, random_sub_goals=False, if_render=True):
+    def __init__(self, map_paths, reward_wrights, max_steps, skip_frames=5, if_render=True):
         self.games:list[Game] = []
         self.map_paths = map_paths
         self.reward_wrights = reward_wrights
@@ -17,9 +17,6 @@ class GameEnv:
         self.num_envs = len(self.games)
         self.skip_frames = skip_frames
         self.action_lists_record = dict()
-        self.sub_goals = []
-        self.random_sub_goals = random_sub_goals
-        self.all_random_sub_goals = []
         if if_render:
             h = self.games[0].height
             w = self.games[0].width
@@ -60,13 +57,6 @@ class GameEnv:
                     game.begin_attack(unit_pos, target_pos)
             reward,done,winner = game.run()
             state = game.get_grid_state()
-
-            if self.random_sub_goals:
-                goal_state = game.get_unit_pos_vector()
-                sub_goals = self.all_random_sub_goals[i]
-                for sub_goal in sub_goals:
-                    if np.array_equal(goal_state, sub_goal):
-                        reward += 1
 
             if done:
                 state = game.reset()
@@ -182,33 +172,3 @@ class GameEnv:
             action_mask = game.get_vector_action_mask(units[i], player_id)
             action_masks.append(action_mask)
         return np.array(action_masks)
-
-    def get_random_sub_goals(self,n_u:int,n_g:int):
-        self.all_random_sub_goals = []
-        for game in self.games:
-            sub_goals = game.get_random_sub_goals(n_u,n_g)
-            self.all_random_sub_goals.append(sub_goals)
-    
-
-if __name__ == "__main__":
-    rewards_wrights = {'win': 10, 'harvest': 1, 'return': 1, 'produce': 1, 'attack': 1}
-    num_envs = 1
-    map_paths = ['maps\\16x16\\basesWorkers16x16.xml' for _ in range(num_envs)]
-    max_steps=5000
-    env = GameEnv(map_paths, rewards_wrights, max_steps, record_actions=True)
-    
-    width = 16
-    height = 16
-    ai0 = RushAI(0, "Random", width, height)
-    ai1 = RushAI(1, "Light", width, height)
-
-    r = Render(16,16)
-
-    for _ in range(100000):
-        r.draw(env.games[0])
-        game = env.games[0]
-        # action: Action(unit_pos:int, action_type:str, target_pos:int, produced_unit_type:UnitType=None)
-        action0 = ai0.get_action(game)
-        action1 = ai1.get_action(game)
-        #action_lists: List[List[Action]]
-        states, rewards, dones, winners = env.one_game_step(action0, action1)
